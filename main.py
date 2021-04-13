15d99ee4136fd841882349591943a9f0b1c2cc78
xanxustyle <hui.lim@utas.edu.au>
2021-04-13 13:27:33 +1000

    Added ui real-time plot

--- main.py
+++ main.py
@@ -47,14 +47,13 @@ def createbin(xmin, xmax, ymin, ymax, edge):
 
 
 class Reader(QtCore.QThread):
-    data_signal = QtCore.pyqtSignal(object)
+    data_signal = QtCore.pyqtSignal(tuple)
     bin_signal = QtCore.pyqtSignal(tuple)
 
     def __init__(self, feature):
         super().__init__()
         self.feature = feature
         self.line_no = 0
-        self.first = True
         self.bin = ()
         self.data = []
 
@@ -65,18 +64,20 @@ class Reader(QtCore.QThread):
         self.line_no += 1
         print(self.line_no, 'lines copied.')
 
-        if self.first:
+        if self.line_no == 1:
             xmin, ymin = newdata[:, :2].min(axis=0) - 1000
             xmax, ymax = newdata[:, :2].max(axis=0) + 1000
             self.bin = createbin(xmin, xmax, ymin, ymax, self.feature)
-            self.first = False
             self.bin_signal.emit(self.bin)
 
         newdata[:, 0] = np.searchsorted(self.bin[0], newdata[:, 0], side='right')
         newdata[:, 1] = np.searchsorted(self.bin[1], newdata[:, 1], side='right')
         newdata = newdata[pd.DataFrame(newdata[:, :2]).drop_duplicates().index]
         self.data.append(newdata)
-        self.data_signal.emit(self.data)
+        np_data = np.vstack(self.data)
+        hist = np.histogram2d(np_data[:, 0] - 0.5, np_data[:, 1] - 0.5, bins=(np.arange(self.bin[0].shape[0]),
+                                                                              np.arange(self.bin[1].shape[0])))[0]
+        self.data_signal.emit((np_data, hist.T))
 
 
 class Window(QMainWindow, Ui_MainWindow):
@@ -92,85 +93,56 @@ class Window(QMainWindow, Ui_MainWindow):
         self.reader = Reader(self.feature)
         self.watcher.watch_signal.connect(self.reader.readline)
         self.reader.bin_signal.connect(self.setbin)
-        self.reader.data_signal.connect(self.drawcovermap)
+        self.reader.data_signal.connect(self.drawmap)
 
-        self.canvas1 = None
-        self.canvas2 = None
-        self.toolbar1 = None
-        self.toolbar2 = None
+        self.fig1 = plt.figure()
+        self.ax1 = self.fig1.add_axes((0, 0, 1, 1), xlabel='Easting [m]', ylabel='Northing [m]', title='Coverage Map',
+                                      aspect='equal', xticks=[], yticks=[], rasterized=True)
+        self.ax1.format_coord = lambda x, y: f"x={x:.2f}, y={y:.2f}"
+        self.cmesh = None
+        self.cbar = None
+        self.canvas1 = FigCanvas(self.fig1)
+        self.plotLayout1.addWidget(self.canvas1)
+        self.plotLayout1.addWidget(FigNavToolbar(self.canvas1, self.plotBox1, coordinates=True))
+        self.fig2 = plt.figure()
+        self.canvas2 = FigCanvas(self.fig2)
+        self.plotLayout2.addWidget(self.canvas2)
+        self.plotLayout2.addWidget(FigNavToolbar(self.canvas2, self.plotBox2, coordinates=True))
 
     @QtCore.pyqtSlot(tuple)
     def setbin(self, tup):
         self.bin = tup
 
-    @QtCore.pyqtSlot(object)
-    def drawcovermap(self, datalist):
-        data = np.vstack(datalist)
-        hist = np.histogram2d(data[:, 0] - 0.5, data[:, 1] - 0.5, bins=(np.arange(self.bin[0].shape[0]),
-                                                                        np.arange(self.bin[1].shape[0])))[0]
-        xedge, yedge = np.meshgrid(self.bin[0], self.bin[1])
-        fig1 = plt.figure()
-        ax1 = fig1.add_axes((0, 0, 1, 1), xlabel='Easting [m]', ylabel='Northing [m]', title='Coverage Map',
-                            aspect='equal', xticks=[], yticks=[], rasterized=True)
-        ax1.set_xlim(self.bin[2][int(data[:, 0].min()) - 5], self.bin[2][int(data[:, 0].max()) + 5])
-        ax1.set_ylim(self.bin[3][int(data[:, 1].min()) - 5], self.bin[3][int(data[:, 1].max()) + 5])
-        ax1.format_coord = lambda x, y: f"x={x:.2f}, y={y:.2f}"
-        cmax = int(np.max(hist))
-        cmap = plt.get_cmap('viridis', cmax + 1)
-        cmesh = ax1.pcolormesh(xedge, yedge, hist.T, cmap=cmap)
-        cbar = fig1.colorbar(cmesh, ticks=np.linspace(cmax / (cmax + 1) / 2, cmax - cmax / (cmax + 1) / 2, cmax + 1),
-                             aspect=50, location='bottom')
-        cbar.ax.set_xticklabels(np.arange(cmax + 1))
-        ax1.set_position((0.05, 0.1, 0.9, 0.9))
-        cbar.ax.set_position((0.05, 0.03, 0.9, 0.018))
-        self.addplot1(fig1)
-        self.addplotool1()
-        self.addplot2(fig1)
-        self.addplotool2()
-
-    def addplot1(self, fig):
-        self.canvas1 = FigCanvas(fig)
-        self.plotLayout1.addWidget(self.canvas1)
+    @QtCore.pyqtSlot(tuple)
+    def drawmap(self, tup):
+        data = tup[0]
+        hist = tup[1]
+        self.ax1.set_xlim(self.bin[2][int(data[:, 0].min()) - 5], self.bin[2][int(data[:, 0].max()) + 5])
+        self.ax1.set_ylim(self.bin[3][int(data[:, 1].min()) - 5], self.bin[3][int(data[:, 1].max()) + 5])
+        if self.cmesh is None:
+            cmax = int(np.max(hist))
+            self.cmesh = self.ax1.imshow(hist, cmap=plt.get_cmap('viridis', cmax + 1), interpolation='nearest',
+                                         origin='lower',
+                                         extent=[self.bin[0][0], self.bin[0][-1], self.bin[1][0], self.bin[1][-1]])
+            self.cbar = self.fig1.colorbar(self.cmesh, ticks=
+                                           np.linspace(cmax / (cmax + 1) / 2, cmax - cmax / (cmax + 1) / 2, cmax + 1),
+                                           aspect=50, location='bottom')
+            self.cbar.ax.set_xticklabels(np.arange(cmax + 1))
+            self.ax1.set_position((0.1, 0.1, 0.85, 0.85))
+            self.cbar.ax.set_position((0.1, 0.03, 0.85, 0.017))
+        else:
+            cmax = int(np.max(hist))
+            self.cmesh.set_data(hist)
+            self.cmesh.set_cmap(plt.get_cmap('viridis', cmax + 1))
+            self.cmesh.autoscale()
+            self.cbar.set_ticks(np.linspace(cmax / (cmax + 1) / 2, cmax - cmax / (cmax + 1) / 2, cmax + 1))
+            self.cbar.draw_all()
+            self.cbar.ax.set_xticklabels(np.arange(cmax + 1))
         self.canvas1.draw()
-
-    def addplot2(self, fig):
-        self.canvas2 = FigCanvas(fig)
-        self.plotLayout2.addWidget(self.canvas2)
-        self.canvas2.draw()
-
-    def addplotool1(self):
-        self.toolbar1 = FigNavToolbar(self.canvas1,
-                                      self.plotBox1, coordinates=True)
-        self.plotLayout1.addWidget(self.toolbar1)
-
-    def addplotool2(self):
-        self.toolbar2 = FigNavToolbar(self.canvas2,
-                                      self.plotBox2, coordinates=True)
-        self.plotLayout2.addWidget(self.toolbar2)
-
-    def delplot1(self, ):
-        self.plotLayout1.removeWidget(self.canvas1)
-        self.canvas1.close()
-
-    def delplot2(self, ):
-        self.plotLayout2.removeWidget(self.canvas2)
-        self.canvas2.close()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = Window()
-
-    # fig1 = Figure()
-    # ax1f1 = fig1.add_subplot(111)
-    # ax1f1.plot(np.random.rand(5))
-    # fig2 = Figure()
-    # ax1f2 = fig2.add_subplot(111)
-    # ax1f2.plot(np.random.rand(5))
-    # win.addplot1(fig1)
-    # win.addplot2(fig2)
-    # win.addplotool1()
-    # win.addplotool2()
-
     win.show()
     sys.exit(app.exec_())
