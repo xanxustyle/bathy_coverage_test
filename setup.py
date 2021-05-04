e9eb507f5dcd44ee8089ae302668c47f4f3d464d
limhs <hui.lim@utas.edu.au>
2021-05-04 12:59:21 +1000

    Version 0.2

--- setup.py
+++ setup.py
@@ -5,20 +5,18 @@ from cx_Freeze import setup, Executable
 PYTHON_INSTALL_DIR = os.path.dirname(sys.executable)
 
 build_exe_options = {
-    'packages': ['numpy', 'shapely', 'osgeo'],
-    'includes': ['scipy.spatial.transform._rotation_groups', 'osgeo.osr', 'osgeo.gdal'],
+    'packages': ['numpy', 'shapely', 'rasterio'],
+    'includes': ['scipy.spatial.transform._rotation_groups'],
     'include_files': []
 }
 
 base = None
 if sys.platform == 'win32':
     base = 'Win32GUI'
-    DLLS_FOLDER = os.path.join(PYTHON_INSTALL_DIR, 'Library', 'bin')
-
-    dependencies = ['mkl_core.dll', 'mkl_def.dll', 'mkl_intel_thread.dll', 'geos_c.dll']
-
-    for dependency in dependencies:
-        build_exe_options['include_files'].append(os.path.join(DLLS_FOLDER, dependency))
+    # DLLS_FOLDER = os.path.join(PYTHON_INSTALL_DIR, 'Library', 'bin')
+    # dependencies = ['mkl_core.dll', 'mkl_def.dll', 'mkl_intel_thread.dll', 'geos_c.dll']
+    # for dependency in dependencies:
+    #     build_exe_options['include_files'].append(os.path.join(DLLS_FOLDER, dependency))
 
 setup(
     name='CoverTest',
