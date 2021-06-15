5f1fb9cf04f38e65b4ef03a3ec11056bc7f264f9
xanxustyle <hui.lim@utas.edu.au>
2021-06-15 16:51:21 +1000

    Version 0.3

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ class Reader(QObject):
                            range=[[self.bin[0][blim[2, 0]], self.bin[0][blim[3, 0]]],
                                   [self.bin[1][blim[2, 1]], self.bin[1][blim[3, 1]]]]) >= self.density, 1, 0)
 
-        self.depth[0] = (data[:, 2].sum() + self.depth[0] * self.depth[1]) / (data.shape[0] + self.depth[1])
+        self.depth[0] = (abs(data[:, 2]).sum() + self.depth[0] * self.depth[1]) / (data.shape[0] + self.depth[1])
         self.depth[1] += data.shape[0]
         self.data_signal.emit((self.hist, self.depth[0], blim[:2, :]))
 
@@ -215,7 +215,7 @@ class Planner(QObject):
         swath_radius = tup[1] * np.tan(np.deg2rad(tup[2] / 2))
         run_iter = tup[3]
 
-        fbin = createbin(fail_xy.min(axis=0), fail_xy.max(axis=0), swath_radius / 2)
+        fbin = createbin(fail_xy.min(axis=0), fail_xy.max(axis=0), swath_radius / 5)
         fail_tree = KDTree(fail_xy)
         fail_grp = np.column_stack((np.searchsorted(fbin[0], fail_xy[:, 0], side='right'),
                                     np.searchsorted(fbin[1], fail_xy[:, 1], side='right')))
@@ -387,7 +387,7 @@ class Window(QMainWindow, Ui_MainWindow):
         self.plotLayout1.addWidget(FigNavToolbar(self.canvas1, self.plotBox1, coordinates=True))
         self.fig2 = Figure()
         self.ax2 = self.fig2.add_axes((0.07, 0.1, 0.9, 0.85), xlabel='Easting [m]', ylabel='Northing [m]',
-                                      title='Grid Compliance', aspect='equal',
+                                      title='Grid Compliance Map', aspect='equal',
                                       xticks=[], yticks=[], rasterized=True)
         self.ax2.set_anchor('C')
         self.linepatch = Polypatch([[0, 0], [0, 0]], edgecolor='black', facecolor='None', lw=0.5, zorder=0)
@@ -612,9 +612,10 @@ class Window(QMainWindow, Ui_MainWindow):
     def drawbound(self, boun_xy):
         self.consoleBox.insertPlainText('Done.')
         self.boundary = boun_xy
-        # noinspection PyArgumentList
         self.linepatch.set_xy(boun_xy)
         self.canvas2.draw()
+        # self.ax1.plot(boun_xy[:,0], boun_xy[:,1], c='red', lw=2, zorder=1)
+        # self.canvas1.draw()
         if self.failGroup.isChecked():
             self.run_checker()
         else:
@@ -629,7 +630,6 @@ class Window(QMainWindow, Ui_MainWindow):
         failrate = min(failarea / bounarea, 1)
         self.failtext.set_text(
             'Compliant Grids (Green): {:.2%}\nNon-compliant Grids (Red): {:.2%}'.format(1 - failrate, failrate))
-        # noinspection PyArgumentList
         self.polypatch.set_xy(self.boundary)
         self.failplot.set_data(fail_xy[:, 0], fail_xy[:, 1])
         self.canvas2.draw()
@@ -695,6 +695,7 @@ if __name__ == '__main__':
     font = QFont()
     font.setPointSize(8)
     app.setFont(font)
+    app.setStyle('Fusion')
     win = Window()
     win.show()
     sys.exit(app.exec_())
